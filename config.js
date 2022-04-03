502156775f3fb9248f31ce1d5edb7670f4a7c226
httpcode <blockchaindevweb@gmail.com>
2022-04-03 10:41:40 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,41 @@
+// Configuration
+window.Config = {
+
+  // Site Name
+  SiteName: 'Uptime Status',
+
+  // Site URL
+  SiteUrl: '/',
+
+  // UptimeRobot Api 域名
+  // Enter domain name part. default isuptimerobot.com's domain
+  // Uptimerobot's api sometimes will report CROS error, you can fill in you own reverse proxy domain name
+  // more details are from https://github.com/yb/uptime-status/ 
+  ApiDomain: 'api.uptimerobot.com',
+  
+  // UptimeRobot Api Keys
+  // Support both Monitor-Specific and  Read-Only type's Api Key
+  ApiKeys: [
+    'ur1685428-d4499ef37acfd86e77bfe528',
+  ],
+
+  // if shows monitored site's URL
+  ShowLink: false,
+
+  // Show how many days of log. 
+  // API can have 90 days, although free plan says only for 60 days.
+  // 60 is the free plan's number for log days. 
+  CountDays: 90,
+
+  // Navigation menu at top right. 
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://51sec.org/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/51sec/uptime-status'
+    }
+  ]
+};
