c34bbf6b24e2843461f44a9367c79c63441d8880
httpcode <blockchaindevweb@gmail.com>
2022-04-03 11:08:39 +0200

    hotfix: feature monitoring api key done

--- config.js
+++ config.js
@@ -16,9 +16,11 @@ window.Config = {
   // UptimeRobot Api Keys
   // Support both Monitor-Specific and  Read-Only type's Api Key
   ApiKeys: [
-    'm783399407-b5654044806678e2a377de7f',
-    'm783399409-ec794f627ab64b259503f034',
-    'm783491939-22f42834a494fc3eadf69059',
+    'm791244724-b42a93574ec7c6a5ea809b73',
+    'm791267304-951c351f521da117c47eab8d',
+    'm791267317-9ee1c697360fb8849c0e3bb5',
+    'm791267349-71f3642ebc5d2c24339b3771',
+    'm791267367-293b03b00905556a16df97f4',
   ],
 
   // if shows monitored site's URL
