6ca09183c8c9e5fa10aa6d2a3f32b802a1a77fe7
httpcode <blockchaindevweb@gmail.com>
2022-04-03 18:50:00 +0200

    hotfix: add build folder

---
+++ build/precache-manifest.1ba04d0590ad0cc1382f7c89a9b2a00c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a29809e7e6738eaeb2d93ffde236e7b5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7b79670ad565aa02ba78",
+    "url": "./static/css/main.707da643.chunk.css"
+  },
+  {
+    "revision": "d23931cd9abd60c50e25",
+    "url": "./static/js/2.f145414a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f145414a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b79670ad565aa02ba78",
+    "url": "./static/js/main.d50dd8a1.chunk.js"
+  },
+  {
+    "revision": "97996f16af1be93b0848",
+    "url": "./static/js/runtime-main.f3a6a7a4.js"
+  },
+  {
+    "revision": "b132844f98b3483f102a78241ad6af1e",
+    "url": "./static/media/background.b132844f.png"
+  },
+  {
+    "revision": "23ef1e5041b1163bd5a47a20428e71df",
+    "url": "./static/media/fail.23ef1e50.svg"
+  },
+  {
+    "revision": "10f6abb42bf0034e508722442d75623c",
+    "url": "./static/media/link.10f6abb4.svg"
+  },
+  {
+    "revision": "6b2b4dd672db38e162514bf6bd4b6ee6",
+    "url": "./static/media/loading.6b2b4dd6.svg"
+  },
+  {
+    "revision": "1ee16bd23741bdb901a71ccd52d3209d",
+    "url": "./static/media/question.1ee16bd2.svg"
+  },
+  {
+    "revision": "394baa21f5e7111ad0a96a6eb0d28774",
+    "url": "./static/media/success.394baa21.svg"
+  }
+]);
